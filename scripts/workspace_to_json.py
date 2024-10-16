81a75faba6b040fb063aea2fca55db9b739c778a
dash9821 <dkgoheldarshan@gmail.com>
2024-10-16 23:36:05 +0530

    trying out csv

--- scripts/workspace_to_json.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-# Read workspaces from input
-workspaces_input = sys.stdin.read().strip()
-
-# Split the input into a list and create a JSON array
-workspaces_list = workspaces_input.split()
-print(json.dumps(workspaces_list))
